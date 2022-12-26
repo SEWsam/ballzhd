609a060f61e0b834f3689a0808f38bdc3d6b790d
SEWsam <49174351+SEWsam@users.noreply.github.com>
2022-12-25 22:37:35 -0600

    balls

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
-import Balls from './balls.png';
+import Balls1 from './isaiah.png';
+import Balls2 from './david.png';
+
+let currentImage = Balls1
+
+function handleImageClick() {
+  if (currentImage === Balls1) {
+    currentImage = Balls2
+  } else if (currentImage === Balls2) {
+    currentImage = Balls1
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={Balls} className="App-logo" alt="logo" />
+        <img src={currentImage} onClick={handleImageClick} className="App-logo" alt="logo" />
         <p>
           BallzHD
         </p>
