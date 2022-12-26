3cb646f39c479b7956c467f3a8b88de856ccd904
SEWsam <49174351+SEWsam@users.noreply.github.com>
2022-12-25 23:09:57 -0600

    society

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { cursorTo } from 'readline';
 import './App.css';
 import Balls2 from './david.png';
 import Balls1 from './isaiah.jpg';
 
-let currentImage = Balls1
 
-function handleImageClick() {
-  if (currentImage === Balls1) {
-    currentImage = Balls2
-  } else if (currentImage === Balls2) {
-    currentImage = Balls1
-  }
-}
 
 function App() {
+  let usingAltImage = false
+  const [imgSrc, setImgSrc] = useState(Balls1);
+
+  const handleImageClick = () => {
+    if (!usingAltImage) {
+      setImgSrc(Balls2)
+      usingAltImage = true
+    } else {
+      setImgSrc(Balls1)
+      usingAltImage = false
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={currentImage} onClick={handleImageClick} className="App-logo" alt="logo" />
+      <header className="App-header"  onClick={handleImageClick} >
+        <img src={imgSrc}  className="App-logo" alt="logo" />
         <p>
           BallzHD
         </p>
